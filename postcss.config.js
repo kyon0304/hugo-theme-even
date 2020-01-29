ae8286cf74f577d3f6fffa1284d4cfb0079cd868
wangying <wangying02@mskj.com>
2020-01-29 16:15:53 +0800

    [fix] Fix h2 anchor long long bug
    Move toc to left

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
     require('autoprefixer')({
-      browsers: ['> 1%', 'last 2 versions', 'ie >= 9']
+      overrideBrowserslist: ['> 1%', 'last 2 versions', 'ie >= 9']
     })
   ]
 };
